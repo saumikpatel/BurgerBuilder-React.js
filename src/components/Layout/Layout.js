969052a96c90d2bcb4c59e34cc567bf3854e8d75
saumik patel <saumikpatel32@gmail.com>
2020-07-25 17:40:21 -0400

    sidebar implemented

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Aux from '../../hoc/Auxiliary'
 import classes from './Layout.css'
+import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
+class Layout extends Component{
+    state={
+        showsideDrawer:false
+    }
+    sideDrawerClosedHandler=()=>{
+        this.setState({showsideDrawer:false})
 
-const layout =(props)=>(
-    <Aux>
-        <div>Toolbar, SideDrawer, Backdrop</div>
+    }
+
+    sideDrawerToggleHandler=()=>{
+        this.setState((prevState)=>{
+            return{
+            showsideDrawer: !prevState.showsideDrawer
+        }})
+    }
+    render(){
+        return(
+        <Aux>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+        <SideDrawer closed={this.sideDrawerClosedHandler} open={this.state.showsideDrawer}/>
         <main className={classes.Content}>
-            {props.children}
+            {this.props.children}
         </main>
     </Aux>
+        )
+    }
+        
 
-);
+}
+   
 
-export default layout;
+
+
+export default Layout;
